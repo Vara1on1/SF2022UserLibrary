a13ee066e195520568f6e51630c865830ac70cd9
nerooon123 <keleron123@gmail.com>
2023-02-17 12:47:49 +0500

    Добавьте файлы проекта.

---
+++ SF2022UserTest/CalculationsTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace SF2022UserTest
+{
+    [TestClass]
+    public class CalculationsTest
+    {
+        [TestMethod]
+        public void Simple_full_test()
+        {
+            //Arrange
+            TimeSpan[]
+                startTimes = new { TimeSpan(10, 10, 10), TimeSpan(10, 02, 02) };
+                int[] durations= {60,30,10,10,40};
+                TimeSpan beginWorkingTime = ;
+                TimeSpan endWorkingTime =
+                int consultationTime = 
+            //Act
+
+            //Assert
+        }
+    }
+}
---
+++ SF2022UserTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SF2022UserTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SF2022UserTest")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("dc01f218-d145-42f8-b4b1-a4c7bc16df8a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
